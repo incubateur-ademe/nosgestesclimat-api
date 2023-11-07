8405517eab0b9dda4633111bb11e8b1cab0ec3eb
Emile Rolley <emile.rolley@tuta.io>
2023-11-07 17:24:22 +0100

    test(api): improve tests

--- cypress/tests/rules.cy.ts
+++ cypress/tests/rules.cy.ts
@@ -1,20 +1,31 @@
+function assertValidRules(rules: object) {
+  const keys = Object.keys(rules)
+  expect(keys).length.to.be.greaterThan(1)
+  expect(keys).to.contain("bilan")
+  expect(keys).to.contain("logement")
+  expect(keys).to.contain("divers")
+  expect(keys).to.contain("alimentation")
+  expect(keys).to.contain("transport")
+  expect(keys).to.contain("services sociétaux")
+}
+
 context("GET /rules", () => {
   it("GET /latest/fr/FR/rules", () => {
     cy.request("GET", "/latest/fr/FR/rules").then((response) => {
       expect(response.status).to.eq(200)
-      expect(Object.keys(response.body)).length.to.be.greaterThan(1)
+      assertValidRules(response.body)
     })
   })
   it("GET /latest/fr/FR/optim-rules", () => {
     cy.request("GET", "/latest/fr/FR/optim-rules").then((response) => {
       expect(response.status).to.eq(200)
-      expect(Object.keys(response.body)).length.to.be.greaterThan(1)
+      assertValidRules(response.body)
     })
   })
   it("GET /latest/en/FR/optim-rules", () => {
     cy.request("GET", "/latest/en/FR/optim-rules").then((response) => {
       expect(response.status).to.eq(200)
-      expect(Object.keys(response.body)).length.to.be.greaterThan(1)
+      assertValidRules(response.body)
     })
   })
 })
